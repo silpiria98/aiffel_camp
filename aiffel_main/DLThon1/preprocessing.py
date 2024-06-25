7b9cc392f80cea9d1c65e260c1d38a789a1f335a
JooHyun <silpiria98@outlook.com>
2024-06-25 15:51:44 +0900

    add removing stopwords

--- aiffel_main/DLThon1/preprocessing.py
+++ aiffel_main/DLThon1/preprocessing.py
@@ -1,8 +1,8 @@
-# 전처리 함수
-# 구분자 삭제
+#전처리
+# 불용어 처리를 할 경우 stopwords에 단어 리스트를 제공
 import re
 
-def preprocess_sentence(sentence):
+def preprocess_sentence(sentence, stopwords=None):
     # 단어와 구두점(punctuation) 사이의 거리를 만듭니다.
     # 예를 들어서 "I am a student." => "I am a student ."와 같이
     # student와 온점 사이에 거리를 만듭니다.
@@ -14,4 +14,10 @@ def preprocess_sentence(sentence):
     # (a-z, A-Z,가-힣,0-9, ".", "?", "!", ",")를 제외한 모든 문자를 공백인 ' '로 대체합니다.
     sentence = re.sub(r"[^a-zA-Z가-힣0-9\.\?\!,]"," ",sentence)
     sentence = sentence.strip()
+    
+    if stopwords:
+        words = sentence.split()
+        filtered_words = [word for word in words if word not in stopwords]
+        sentence = ' '.join(filtered_words)
+    
     return sentence
