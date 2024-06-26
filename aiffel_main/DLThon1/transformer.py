6e9fc42679fe4605f0214e6c0db3744213618eec
JooHyun <silpiria98@outlook.com>
2024-06-26 10:31:02 +0900

    upload

---
+++ aiffel_main/DLThon1/transformer.py
@@ -0,0 +1,258 @@
+# 하이퍼파라미터
+# NUM_LAYERS = 12 # 인코더와 디코더의 층의 개수 
+# D_MODEL = 128 # 인코더와 디코더 내부의 입/출력의 고정 차원
+# NUM_HEADS = 4 # 멀티 헤드 어텐션에서의 헤드 수 
+# UNITS = 256 # 피드 포워드 신경망의 은닉층의 크기
+# DROPOUT = 0.1 # 드롭아웃의 비율
+
+# NUM_CLASSES = len(class_label)  #레이블 수
+# VOCAB_SIZE = tokenizer.vocab_size #단어사전 크기
+# MAX_LENGTH = X.shape[1] # maxlen
+
+# 포지셔널 인코딩 레이어
+class PositionalEncoding(tf.keras.layers.Layer):
+
+    def __init__(self, position, d_model):
+        super(PositionalEncoding, self).__init__()   # 부모 클래스 상속 초기화, 필수는 아님 
+        self.pos_encoding = self.positional_encoding(position, d_model)
+
+    def get_angles(self, position, i, d_model):
+        angles = 1 / tf.pow(10000, (2 * (i // 2)) / tf.cast(d_model, tf.float32))
+        return position * angles
+
+    def positional_encoding(self, position, d_model):
+        # 각도 배열 생성
+        angle_rads = self.get_angles(
+            # tf.newaxis : 차원 추가, [:, tf.newaxis]은 열벡터로 변환 
+            position=tf.range(position, dtype=tf.float32)[:, tf.newaxis],  
+            i=tf.range(d_model, dtype=tf.float32)[tf.newaxis, :],
+            d_model=d_model)
+
+        # 배열의 짝수 인덱스에는 sin 함수 적용
+        sines = tf.math.sin(angle_rads[:, 0::2])
+        # 배열의 홀수 인덱스에는 cosine 함수 적용
+        cosines = tf.math.cos(angle_rads[:, 1::2])
+
+        # sin과 cosine이 교차되도록 재배열
+        pos_encoding = tf.stack([sines, cosines], axis=0)
+        pos_encoding = tf.transpose(pos_encoding,[1, 2, 0]) 
+        pos_encoding = tf.reshape(pos_encoding, [position, d_model])
+
+        # 차원 추가 (1, ...)
+        pos_encoding = pos_encoding[tf.newaxis, ...]
+        return tf.cast(pos_encoding, tf.float32)
+
+    def call(self, inputs):
+        return inputs + self.pos_encoding[:, :tf.shape(inputs)[1], :]
+    
+    def get_config(self):
+        return {
+            'pos_encoding': self.pos_encoding,
+        }
+        
+def scaled_dot_product_attention(query, key, value, mask):
+    # 어텐션 가중치는 Q와 K의 닷 프로덕트
+    matmul_qk = tf.matmul(query, key, transpose_b=True)
+
+    # 가중치를 정규화
+    # tf.shape(key)[-1] = 워드벡터의 크기 
+    depth = tf.cast(tf.shape(key)[-1], tf.float32)
+    logits = matmul_qk / tf.math.sqrt(depth)
+
+    # 패딩에 마스크 추가
+    # 마스크의 1이면 큰값을 빼는 거니까 softmax를 지나면 0에 수렴 
+    if mask is not None:
+        logits += (mask * -1e9)
+
+    # softmax적용
+    attention_weights = tf.nn.softmax(logits, axis=-1)
+
+    # 최종 어텐션은 가중치와 V의 닷 프로덕트
+    output = tf.matmul(attention_weights, value)
+    return output
+
+class MultiHeadAttention(tf.keras.layers.Layer):
+
+    def __init__(self, d_model, num_heads, name="multi_head_attention"):
+        super(MultiHeadAttention, self).__init__(name=name)
+        self.num_heads = num_heads
+        self.d_model = d_model
+        
+        # 워드벡터가 헤드수로 나머지 없이 나누어 져야함
+        assert d_model % self.num_heads == 0
+
+        self.depth = d_model // self.num_heads
+
+        self.query_dense = tf.keras.layers.Dense(units=d_model)
+        self.key_dense = tf.keras.layers.Dense(units=d_model)
+        self.value_dense = tf.keras.layers.Dense(units=d_model)
+
+        self.dense = tf.keras.layers.Dense(units=d_model)
+
+    def split_heads(self, inputs, batch_size):
+        # input.shape : [batch_size, seq_len, d_model(self.num_head*self.depth)]
+            # seq_len : 문장길이
+            # d_model : 임베딩 차원 
+        inputs = tf.reshape(
+            inputs, shape=(batch_size, -1, self.num_heads, self.depth))
+        #[batch_size, self.num_head, seq_len, self.depth)]
+        # transpose를 통해 num_heads를 앞으로 불러와 병렬연산 효율성(gpu활용성)을 높임 
+            # 일반적으로 뒷 차원의 크기가 클 수록 효율적
+        return tf.transpose(inputs, perm=[0, 2, 1, 3])
+
+    def call(self, inputs):
+        query, key, value, mask = inputs['query'], inputs['key'], inputs[
+            'value'], inputs['mask']
+        batch_size = tf.shape(query)[0]
+
+        # Q, K, V에 각각 Dense를 적용합니다
+        query = self.query_dense(query)
+        key = self.key_dense(key)
+        value = self.value_dense(value)
+
+        # 병렬 연산을 위한 머리를 여러 개 만듭니다
+        query = self.split_heads(query,batch_size)
+        key = self.split_heads(key,batch_size)
+        value = self.split_heads(value,batch_size)
+
+        # 스케일드 닷 프로덕트 어텐션 함수
+        scaled_attention = scaled_dot_product_attention(query, key, value, mask)
+
+        scaled_attention = tf.transpose(scaled_attention, perm=[0, 2, 1, 3])
+
+        # 어텐션 연산 후에 각 결과를 다시 연결(concatenate)합니다
+        concat_attention = tf.reshape(scaled_attention,
+                                      (batch_size, -1, self.d_model))
+
+        # 최종 결과에도 Dense를 한 번 더 적용합니다
+        outputs = self.dense(concat_attention)
+
+        return outputs
+
+    def get_config(self):
+        return {
+            'num_heads': self.num_heads,
+            'd_model': self.d_model,
+            'depth': self.depth,
+            'query_dense': self.query_dense,
+            'key_dense': self.key_dense,
+            'value_dense': self.value_dense,
+            'dense': self.dense,
+        }
+
+# 숫자가 0인 부분을 체크한 벡터를 리턴
+def create_padding_mask(x):
+    mask = tf.cast(tf.math.equal(x, 0), tf.float32)
+    # (batch_size, 1, 1, sequence length)
+    return mask[:, tf.newaxis, tf.newaxis, :]
+
+# 숫자가 0인 부분도 마스킹
+def create_look_ahead_mask(x):
+    seq_len = tf.shape(x)[1]
+    # tf.linalg.band_part: 대각요소를 가져오거나 이를 기준을 다른 것을 제거 
+        # (텐서, 유지할 아래 대각요소, 유지할 위 대각요소) -1이면 전부 
+        # 대각요소는 항상 유지 
+    look_ahead_mask = 1 - tf.linalg.band_part(tf.ones((seq_len, seq_len)), -1, 0)
+    padding_mask = create_padding_mask(x)
+    return tf.maximum(look_ahead_mask, padding_mask)
+
+# 인코더 하나의 레이어를 함수로 구현.
+# 이 하나의 레이어 안에는 두 개의 서브 레이어가 존재합니다.
+def encoder_layer(units, d_model, num_heads, dropout, name="encoder_layer"):
+    inputs = tf.keras.Input(shape=(None, d_model), name="inputs")
+
+    # 패딩 마스크 사용
+    padding_mask = tf.keras.Input(shape=(1, 1, None), name="padding_mask")
+
+    # 첫 번째 서브 레이어 : 멀티 헤드 어텐션 수행 (셀프 어텐션)
+    attention = MultiHeadAttention(
+      d_model, num_heads, name="attention")({
+          'query': inputs,
+          'key': inputs,
+          'value': inputs,
+          'mask': padding_mask
+      })
+
+    # 어텐션의 결과는 Dropout과 Layer Normalization이라는 훈련을 돕는 테크닉을 수행
+    attention = tf.keras.layers.Dropout(rate=dropout)(attention)
+    attention = tf.keras.layers.LayerNormalization(
+      epsilon=1e-6)(inputs + attention)
+
+    # 두 번째 서브 레이어 : 2개의 완전연결층
+    outputs = tf.keras.layers.Dense(units=units, activation='relu')(attention)
+    outputs = tf.keras.layers.Dense(units=d_model)(outputs)
+
+    # 완전연결층의 결과는 Dropout과 LayerNormalization이라는 훈련을 돕는 테크닉을 수행
+    outputs = tf.keras.layers.Dropout(rate=dropout)(outputs)
+    outputs = tf.keras.layers.LayerNormalization(
+      epsilon=1e-6)(attention + outputs)
+
+    return tf.keras.Model(
+      inputs=[inputs, padding_mask], outputs=outputs, name=name)
+
+# 전체 인코더. 인코딩 레이어를 여러개 
+def encoder(vocab_size,
+            num_layers,
+            units,
+            d_model,
+            num_heads,
+            dropout,
+            name="encoder"):
+    inputs = tf.keras.Input(shape=(None,), name="inputs")
+
+    # 패딩 마스크 사용
+    padding_mask = tf.keras.Input(shape=(1, 1, None), name="padding_mask")
+
+    # 임베딩 레이어
+    embeddings = tf.keras.layers.Embedding(vocab_size, d_model)(inputs)
+    embeddings *= tf.math.sqrt(tf.cast(d_model, tf.float32))
+
+    # 포지셔널 인코딩
+    embeddings = PositionalEncoding(vocab_size, d_model)(embeddings)
+
+    outputs = tf.keras.layers.Dropout(rate=dropout)(embeddings)
+
+    # num_layers만큼 쌓아올린 인코더의 층.
+    for i in range(num_layers):
+        outputs = encoder_layer(
+            units=units,
+            d_model=d_model,
+            num_heads=num_heads,
+            dropout=dropout,
+            name="encoder_layer_{}".format(i),
+        )([outputs, padding_mask])
+
+    return tf.keras.Model(
+      inputs=[inputs, padding_mask], outputs=outputs, name=name)
+
+def transformer(vocab_size,
+                num_layers,
+                units,
+                d_model,
+                num_heads,
+                dropout,
+                num_classes,
+                name="transformer"):
+    inputs = tf.keras.Input(shape=(None,), name="inputs")
+  
+    # 인코더에서 패딩을 위한 마스크
+    # output_shape:mask[:, tf.newaxis, tf.newaxis, :]를 (1,1,batch_size,sequence_length)로 변경
+    enc_padding_mask = tf.keras.layers.Lambda(
+        create_padding_mask, output_shape=(1, 1, None), 
+        name='enc_padding_mask')(inputs)
+
+    # 인코더
+    enc_outputs = encoder(
+        vocab_size=vocab_size,
+        num_layers=num_layers,
+        units=units,
+        d_model=d_model, 
+        num_heads=num_heads,
+        dropout=dropout,
+    )(inputs=[inputs, enc_padding_mask])
+
+    # 완전연결층
+    outputs = tf.keras.layers.GlobalAveragePooling1D()(enc_outputs)
+    outputs = tf.keras.layers.Dense(num_classes, activation='softmax')(outputs)
+
+    return tf.keras.Model(inputs=inputs, outputs=outputs, name=name)
