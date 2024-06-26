c6274face98ab3351696ccba5e3f74c411af8161
JooHyun <silpiria98@outlook.com>
2024-06-26 19:31:27 +0900

    remove '키키', '키키키'...

--- aiffel_main/DLThon1/preprocessing.py
+++ aiffel_main/DLThon1/preprocessing.py
@@ -17,18 +17,21 @@ def preprocess_sentence(sentence, stopwords=None):
     # 단어와 구두점(punctuation) 사이의 거리를 만듭니다.
     # 예를 들어서 "I am a student." => "I am a student ."와 같이
     # student와 온점 사이에 거리를 만듭니다.
-    sentence = sentence.replace("\n", "")         # 구분자
-    sentence = sentence.replace("\r", "")         # 구분자
+    sentence = sentence.replace("\n", "")  # 구분자
+    sentence = sentence.replace("\r", "")  # 구분자
     sentence = re.sub(r"([?.!,])", r" \1 ", sentence)
     sentence = re.sub(r'[" "]+', " ", sentence)
 
     # (a-z, A-Z,가-힣,0-9, ".", "?", "!", ",")를 제외한 모든 문자를 공백인 ' '로 대체합니다.
-    sentence = re.sub(r"[^a-zA-Z가-힣0-9\.\?\!,]"," ",sentence)
+    sentence = re.sub(r"[^a-zA-Z가-힣0-9\.\?\!,]", " ", sentence)
     sentence = sentence.strip()
-    
+
+    # '키키'와 같이 연속된 키를 제거합니다.
+    sentence = re.sub(r'키{2,}', '', sentence)
+
     if stopwords:
         words = sentence.split()
         filtered_words = [word for word in words if word not in stopwords]
         sentence = ' '.join(filtered_words)
-    
+
     return sentence
